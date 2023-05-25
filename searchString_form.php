8d4302f0d6c2f3757e11402b30756d6747952367
juHackWeg <justin-lind@web.de>
2023-05-25 14:51:03 +0200

    Removed form

--- searchString_form.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class block_testblock_edit_form extends block_edit_form {
-    protected function specific_definition($mform) {
-        $mform->addElement('text', 'config_searchstring', get_string('searchstring', 'block_testblock'));
-        $mform->setType('config_searchstring', PARAM_TEXT);
-    }
-}
